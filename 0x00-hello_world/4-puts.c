f480e17ed697930e8d4fb28bb0731659c8de8b28
abubakrali10 <bakrali175@yahoo.com>
2023-02-16 17:36:11 -0800

    a C program that print one line using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - A program that return a line of string using puts function
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
