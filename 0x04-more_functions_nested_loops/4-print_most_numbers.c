48c6e9591a44d7695141c539704ef11111533a5f
abubakrali10 <bakrali175@yahoo.com>
2023-02-23 13:42:31 -0800

    function that prints the numbers, from 0 to 9 but not 2 or 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - Entry point
+  * Return: numbers from 0 to 9 but not 2 or 4
+  */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
