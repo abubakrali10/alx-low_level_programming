76f7726bc33bc96314618e0862b3a4c18b6cd1bd
abubakrali10 <bakrali175@yahoo.com>
2023-02-23 12:09:39 -0800

    function that return multiplication of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * mul - Entry point
+  * @a: first number
+  * @b: second number
+  * Return: multiplication of first number and second number
+  */
+
+int mul(int a, int b)
+{
+	int result;
+	
+	result = a * b;
+	return (result);
+}
