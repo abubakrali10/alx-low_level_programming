51c5d9e43e19b1e592b2002271710f6e7b471d0c
abubakrali10 <bakrali175@yahoo.com>
2023-03-08 15:39:06 -0800

    a recursive function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -24,11 +24,11 @@ int wildcmp(char *s1, char *s2)
 			}
 			s1++;
 		}
-		return (wildcmp(s1, s2);
+		return (wildcmp(s1, s2));
 	}
 	if (*s1 == *s2)
 	{
-		return (wildcmp(s1 + 1, s2 + 1);
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
 }
