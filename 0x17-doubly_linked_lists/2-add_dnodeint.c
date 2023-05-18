a9b546c004fafc8504bfdc5d3cb359669d16017f
abubakrali10 <bakrali175@yahoo.com>
2023-05-18 13:28:27 -0700

    fixed code by deleting code dublication and make it more readable

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,36 +8,28 @@
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
 	if (*head == NULL)
 	{
-		dlistint_t *first_node = malloc(sizeof(dlistint_t));
-
-		if (first_node == NULL)
-			return (NULL);
-
-		*head = first_node;
-		first_node->n = n;
-		first_node->prev = NULL;
-		first_node->next = NULL;
-
-		return (first_node);
+		*head = new_node;
+		new_node->prev = NULL;
+		new_node->next = NULL;
 	}
 	else
 	{
-		dlistint_t *new_node = malloc(sizeof(dlistint_t));
-
-		if (new_node == NULL)
-			return (NULL);
-
 		dlistint_t *temp = NULL;
 
 		temp = *head;
 		*head = new_node;
-		new_node->n = n;
 		new_node->prev = NULL;
 		new_node->next = temp;
 		temp->prev = *head;
-
-		return (new_node);
 	}
+
+	return (new_node);
 }
