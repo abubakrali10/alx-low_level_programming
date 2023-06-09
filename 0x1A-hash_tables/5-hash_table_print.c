959965d2e7f46db3edba442d79527c901d6d139a
abubakrali10 <bakrali175@yahoo.com>
2023-06-09 12:10:39 -0700

    a function that prints a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints hash table
+ * @ht: the hashtable
+ * Return: nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, j = 0;
+	hash_node_t *traverse = NULL;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		traverse = ht->array[i];
+
+		while (traverse)
+		{
+			if (j == 1)
+				printf(", ");
+			j = 1;
+
+			printf("'%s' : '%s'", traverse->key, traverse->value);
+			traverse = traverse->next;
+		}
+	}
+	printf("}\n");
+}
